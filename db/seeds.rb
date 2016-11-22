9b76684b696c8a1c7f9a24f4d9b51ac67d867434
cassaram09 <cassaram09@gmail.com>
2016-11-22 12:01:09 -0500

    update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,23 +9,13 @@
 @matt = User.create(name: "Matt", username: "cassaram09", password: "hello123", email: "matt@matt.com")
 
 @joe = Contact.create(name: "Joe Pisciotta", email: "joe@redhat.com", job_title: "Platform Consultant", job_description: "does OS and platfomr things",
-                connection_degree: "First", connection_strength: "Moderate" )
+                connection_degree: "First", connection_strength: "Moderate", user: @matt )
 
-@redhat = Company.create(name: "Redhat", industry: "Cloud Hosting", size: "Large", website: "http://www.redhat.com")
+@redhat = Company.create(name: "Redhat", industry: "Cloud Hosting", size: "Large", website: "http://www.redhat.com", user: @matt, contacts: [@joe])
 
-@web_developer = Job.create(job_title: "Web Developer", job_description: "front end web developer")
+@web_developer = Job.create(job_title: "Web Developer", job_description: "front end web developer", company: @redhat, referrer: @joe, user: @matt)
 
 @meet = JobAction.create()
 
-@joe.company = @redhat
-@joe.user = @matt
-@joe.save
-
-@redhat.user = @matt 
-@redhat.jobs << @web_developer
-@redhat.save
-
-@web_developer.referrer = @joe 
-@web_developer.save
 
 

4ba692ba6d69c36539a36df6167ee970fc78a1bd
cassaram09 <cassaram09@gmail.com>
2016-11-22 10:19:51 -0500

    add name and username parameters for User model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar, :name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name])
+  end
 end

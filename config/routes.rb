ec45a04e33b1e66a6421041a400ea5b4c6b6e0ec
cassaram09 <cassaram09@gmail.com>
2016-11-22 12:03:50 -0500

    update standard devise routes with user friendly ones

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
+
   resources :job_actions
   resources :jobs
   resources :companies
   resources :contacts
+  
+
+  devise_scope :user do
+    get '/login' => 'devise/sessions#new'
+    post '/login' => 'devise/sessions#create'
+    delete '/logout' => 'devise/sessions#destroy'
+    get "/signup" => "devise/registrations#new"
+    get "/profile" => 'devise/registrations#edit'
+  end
+
   devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

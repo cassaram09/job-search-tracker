e3f29fd45df409775d2a9eaad3d066fea521f532
cassaram09 <cassaram09@gmail.com>
2016-11-22 11:32:29 -0500

    add JobAction migration and model attributes

--- app/models/job_action.rb
+++ app/models/job_action.rb
@@ -1,2 +1,6 @@
 class JobAction < ApplicationRecord
+  belongs_to :company
+  belongs_to :contact
+  enum action: ['networking', 'job application', 'git commits', 'blog post']
+  belongs_to :job
 end
--- db/migrate/20161122150657_create_job_actions.rb
+++ db/migrate/20161122150657_create_job_actions.rb
@@ -1,6 +1,11 @@
 class CreateJobActions < ActiveRecord::Migration[5.0]
   def change
     create_table :job_actions do |t|
+      t.integer :action
+      t.belongs_to :company
+      t.belongs_to :contact
+      t.belongs_to :user
+      t.belongs_to :job
 
       t.timestamps
     end

919eed7bdc0e481908509408e94c78bb08790393
cassaram09 <cassaram09@gmail.com>
2016-11-22 10:16:58 -0500

    add activerecord relationships for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :contacts
+  has_many :companies
+  has_many :jobs, through: :companies
+  has_many :job_actions
+  
 end
